8a7eaac3d25a74b37941067253ca92d3248a37ca
Lukas SP <lukaesebrot@gmail.com>
2020-05-31 02:39:47 +0200

    Implement credits spent request method

--- client.go
+++ client.go
@@ -1,6 +1,11 @@
 package godoodle
 
-import "github.com/valyala/fasthttp"
+import (
+	"encoding/json"
+	"errors"
+
+	"github.com/valyala/fasthttp"
+)
 
 // Client represents an API client
 type Client struct {
@@ -18,4 +23,44 @@ func New(clientID, clientSecret string) *Client {
 		clientID:     clientID,
 		clientSecret: clientSecret,
 	}
+}
+
+// CreditsSpent checks how much credits the current client already used
+func (client *Client) CreditsSpent() (int64, error) {
+	// Acquire a request object
+	request := fasthttp.AcquireRequest()
+	defer fasthttp.ReleaseRequest(request)
+
+	// Acquire a response object
+	response := fasthttp.AcquireResponse()
+	defer fasthttp.ReleaseResponse(response)
+
+	// Prepare the request object
+	request.SetRequestURI(EndpointCreditsSpent)
+	request.Header.SetMethod("POST")
+	request.Header.SetContentType("application/json")
+	request.PostArgs().Set("clientId", client.clientID)
+	request.PostArgs().Set("clientSecret", client.clientSecret)
+
+	// Perform the request
+	err := client.http.Do(request, response)
+	if err != nil {
+		return -1, err
+	}
+
+	// Parse the response into a json struct
+	var responseStruct struct {
+		Used  int64  `json:"used,omitempty"`
+		Error string `json:"error,omitempty"`
+	}
+	err = json.Unmarshal(response.Body(), &responseStruct)
+	if err != nil {
+		return -1, err
+	}
+
+	// Return the corresponding values
+	if responseStruct.Error != "" {
+		return -1, errors.New(responseStruct.Error)
+	}
+	return responseStruct.Used, nil
 }
---
+++ constants.go
@@ -0,0 +1,7 @@
+package godoodle
+
+// These constants represent the API endpoints
+const (
+	EndpointExecute      = "https://api.jdoodle.com/v1/execute"
+	EndpointCreditsSpent = "https://api.jdoodle.com/v1/credit-spent"
+)
